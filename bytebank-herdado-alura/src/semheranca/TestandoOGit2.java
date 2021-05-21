28d4a2e52c7bbe54b8274b34b13cea53bf7f2cac
milly <milly@DESKTOP-TQG1L6N>
2021-05-21 16:14:33 -0300

    Testando Github 2

---
+++ bytebank-herdado-alura/src/semheranca/TestandoOGit2.java
@@ -0,0 +1,10 @@
+package semheranca;
+
+public class TestandoOGit2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Good morning");
+		System.out.println("Testando o GitHub 2");
+	}
+}
