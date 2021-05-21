6c9fc76d8b072af33086ed293558d955dd7f2ec6
milly <milly@DESKTOP-TQG1L6N>
2021-05-21 16:11:19 -0300

    Classe Testando o github

---
+++ bytebank-herdado-alura/src/semheranca/TestandoOGit.java
@@ -0,0 +1,10 @@
+package semheranca;
+
+public class TestandoOGit {
+
+	public static void main(String[] args) {
+
+		System.out.println("Good morning");
+		System.out.println("Testando o GitHub");
+	}
+}
